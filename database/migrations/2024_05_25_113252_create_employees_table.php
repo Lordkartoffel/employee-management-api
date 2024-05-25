27badfc64cfe7ebb90fa63041a8068ebcd5403b9
Jannik Meier <jannik-m-1@web.de>
2024-05-25 13:41:19 +0200

    created migration for csv

---
+++ database/migrations/2024_05_25_113252_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("user_prefix");
+            $table->string("first_name");
+            $table->string("middle_inital");
+            $table->string("last_name");
+            $table->char("gender");
+            $table->string("email")->unique();
+            $table->date("date_of_birth");
+            $table->timestamp("time_of_birth");
+            $table->integer("age_in_years");
+            $table->date("date_of_joining");
+            $table->integer("age_in_company_years");
+            $table->string("phone_no");
+            $table->string("place_name");
+            $table->string("county");
+            $table->string("county");
+            $table->string("zip");
+            $table->string("region");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
